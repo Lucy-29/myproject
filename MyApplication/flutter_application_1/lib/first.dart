8ba8e36f43ecb19051d08959cf1c4337ad7658da
Lucy-29 <elissaralali37@gmail.com>
2024-12-23 19:29:59 +0000

    send

---
+++ MyApplication/flutter_application_1/lib/first.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("search bar"),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: [
+            MaterialButton(
+              child: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: CostumSearch());
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CostumSearch extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = "";
+          },
+          icon: Icon(Icons.close))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    throw UnimplementedError();
+  }
+}
